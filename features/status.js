3838d63a2cf4f076d73dfc7f5a294a936f3aa05b
Dutched420 <72705964+Dutched420@users.noreply.github.com>
2021-07-17 23:00:45 +0200

    Update status.js

--- features/status.js
+++ features/status.js
@@ -1,10 +1,19 @@
 const ms = require('ms');
 module.exports = (client, instance) => {
     client.on("ready", () =>{
+        
         function randomStatus(){
-         let status = ["prefix = c.", `${client.guilds.cache.get(process.env.serverID).memberCount} members`, `${ms(client.uptime, { long: true })} uptime`, "monke", "cannoningparadise.club", "made by dutch"]
+         let status = 
+         ["prefix = c.", 
+         `${client.guilds.cache.get(process.env.serverID).memberCount} members`, 
+         `${ms(client.uptime, { long: true })} uptime`, 
+         "monke", 
+         "cannoningparadise.club", 
+         "made by dutch"]
+         
          let rstatus = Math.floor(Math.random() * status.length);
          client.user.setActivity(status[rstatus], {type: "STREAMING", url: "https://www.twitch.tv/orh2004"});
+            
              }; 
                  setInterval(randomStatus, 60000)
                  console.log(`${client.user.tag} is online`);    
